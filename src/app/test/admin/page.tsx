b65c7f7e4cc3737a36ba87482e0766ccc062beee
googie08 <sanojezrel5@gmail.com>
2025-03-20 16:22:11 +0800

    Let's all make a pledge

--- src/app/test/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from 'react';
-import CalendarPage from '@/components/custom/CalendarPage';
-
-export default function AdminCalendarPage() {
-  return <CalendarPage isAdmin={true} />;
-}
